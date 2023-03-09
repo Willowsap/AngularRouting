3e341a2e533a1647d1c6464e90ccac01a8ee3457
Willowsap <willowsapphire@protonmail.com>
2023-03-09 11:52:55 -0500

    initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RoutingPractice';
+  @ViewChild(HeaderComponent)
+  header: HeaderComponent | undefined;
 }
